d2a7ff4495231ebe42c155bbbaf6dceb79949155
Bahaaib <Bahaasco@gmail.com>
2019-04-18 19:20:31 +0200

    Rebuilt on MVVM

--- lib/HomePage/home_page_view.dart
+++ lib/HomePage/home_page_view.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:storyteller/card_scroll_widget.dart';
-import 'package:storyteller/data.dart';
-import 'package:storyteller/main.dart';
+import 'dart:math';
 
-class HomePage extends State<MyApp> {
+import 'package:flutter/material.dart';
+import 'package:storyteller/HomePage/home_page_viewmodel.dart';
+import 'package:storyteller/data.dart';
+
+class HomePageView extends HomePageViewModel {
   var currentPage = images.length - 1.0;
 
   @override
@@ -187,6 +188,115 @@ class HomePage extends State<MyApp> {
           ),
         ),
       ),
+    );
+  }
+}
+
+final double cardAspectRatio = 12.0 / 16.0;
+final double widgetAspectRatio = cardAspectRatio * 1.2;
+
+class CardScrollWidget extends StatelessWidget {
+  final currentPage;
+  final padding = 20.0;
+  final verticalInset = 20.0;
+
+  CardScrollWidget(this.currentPage);
+
+  @override
+  Widget build(BuildContext context) {
+    return new AspectRatio(
+      aspectRatio: widgetAspectRatio,
+      child: LayoutBuilder(builder: (context, constraints) {
+        double width = constraints.maxWidth;
+        double height = constraints.maxHeight;
+
+        double safeWidth = width - 2 * padding;
+        double safeHeight = height - 2 * padding;
+
+        double heightOfPrimaryCard = safeHeight;
+        double widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
+
+        double primaryCardLeft = safeWidth - widthOfPrimaryCard;
+        double horizontalInset = primaryCardLeft / 2;
+
+        List<Widget> cardList = new List();
+
+        for (var i = 0; i < images.length; i++) {
+          double delta = i - currentPage;
+          bool isOnRight = delta > 0;
+
+          double start = padding +
+              max(
+                  primaryCardLeft -
+                      horizontalInset * -delta * (isOnRight ? 15 : 1),
+                  0.0);
+
+          var cardItem = Positioned.directional(
+            top: padding + verticalInset * max(-delta, 0.0),
+            bottom: padding + verticalInset * max(-delta, 0.0),
+            start: start,
+            textDirection: TextDirection.rtl,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16.0),
+              child: Container(
+                decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                  BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(3.0, 6.0),
+                      blurRadius: 10.0)
+                ]),
+                child: AspectRatio(
+                  aspectRatio: cardAspectRatio,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      Image.asset(images[i], fit: BoxFit.cover),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 16.0, vertical: 8.0),
+                              child: Text(title[i],
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 25.0,
+                                      fontFamily: "SF-Pro-Text-Regular")),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 12.0, bottom: 12.0),
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 22.0, vertical: 6.0),
+                                decoration: BoxDecoration(
+                                    color: Colors.blueAccent,
+                                    borderRadius: BorderRadius.circular(20.0)),
+                                child: Text("Read Later",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+          cardList.add(cardItem);
+        }
+        return Stack(
+          children: cardList,
+        );
+      }),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:storyteller/home_page.dart';
+import 'package:storyteller/HomePage/home_page.dart';
 
 void main() => runApp(MaterialApp(
-      home: MyApp(),
+  home: StorytellerApp(),
       debugShowCheckedModeBanner: false,
     ));
 
-class MyApp extends StatefulWidget {
+class StorytellerApp extends StatelessWidget {
+  final Widget homePage = new HomePage();
+
   @override
-  HomePage createState() => new HomePage();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Storyteller',
+      routes: <String, WidgetBuilder>{
+        '/home': (BuildContext context) => new HomePage()
+      },
+    );
+  }
 }
