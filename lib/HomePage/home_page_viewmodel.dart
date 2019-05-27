d4f9cf2b270c4458bc398cd1d43268c078b8199a
Bahaaib <Bahaasco@gmail.com>
2019-05-27 14:11:25 +0200

    fixed linter warnings for HomePage ViewModel

--- lib/HomePage/home_page_viewmodel.dart
+++ lib/HomePage/home_page_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:storyteller/HomePage/home_page.dart';
 
+///Home Page ViewModel class
 abstract class HomePageViewModel extends State<HomePage> {}
